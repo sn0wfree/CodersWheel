2b624b94194933c6f8c931189566c8309611e01d
sn0wfree <snowfreedom0815@gmail.com>
2018-06-22 10:33:28 +0800

    add Untested function-Coroutine
    add function CoroutineWheel

---
+++ Service/CoroutineBoostup.py
@@ -0,0 +1,49 @@
+# -*- coding:utf-8 -*-
+
+class CoroutineWheel(object):
+    def __init__(self):
+        self.Coroutine = self.Coroutine_core
+
+    def Initialization(self, *args, **kwargs):
+        # ---- init-----
+        print('---- init-----')
+        status = "init"
+        return status, args, kwargs
+
+    def Processor(self, core, *args,**kwargs):
+        # -----Processor----------
+
+        argss=None
+        status = 'processed'
+        return status, argss
+
+    def StatusBar(self,status,*args,**kwargs):
+        pass
+
+    def Breaktrigger(self, situation):
+        # -----Break Detector------
+        return 'End'
+
+    def Coroutine_core(self, *args,**kwargs):
+        # ---- init-----
+        status, args, kwargs = self.Initialization(*args,**kwargs)
+        # ---- init----- Done
+        while 1:
+            core = yield status
+            # -----Break Detector------
+            if self.Breaktrigger(status) == 'End':
+                break
+            else:
+                pass
+            # -----Break Detector------Done
+
+            # -----Processor----------
+            status, argss = self.Processor(core)
+            # -----Processor---------- Done
+            # -----StatusBar-----------
+            self.StatusBar(status)
+            # -----StatusBar-----------
+
+
+if __name__ == '__main__':
+    pass

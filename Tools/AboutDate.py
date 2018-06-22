5caee8f58887344de5385518fcc4b5b0623b8f42
sn0wfree <snowfreedom0815@gmail.com>
2018-06-22 10:16:15 +0800

    add Datetime Transfor function
    add Datetime Transfor function
    1. str 2 datetime
    2. str 2 timestamp
    3. datetime 2 timestamp
    4. datetime 2 str
    5. timestamp 2 str
    6. timestamp 2 datetime

--- Tools/AboutDate.py
+++ Tools/AboutDate.py
@@ -4,17 +4,11 @@ import calendar
 import time
 import pandas as pd
 
-
-class HandleDate(object):
-    """
-    This class provides the date relative the api service and handle some date
-    format.
-    """
-
+class GetDate(object):
     def __init__(self):
         pass
 
-    def GetNextQuarterByYearMonth(self, YearMonth):
+    def Get_NextQuarterByYearMonth(self, YearMonth):
         """ YearMonth = '1802'"""
         M = YearMonth[2:]
         Y = YearMonth[:2]
@@ -27,7 +21,7 @@ class HandleDate(object):
         elif M <= '12' and M < '03':
             return ('%d03' % (int(Y) + 1), '%d09' % (int(Y) + 1))
 
-    def GetNextQuarterByQuarterNum(self, q):
+    def Get_NextQuarterByQuarterNum(self, q):
         q = int(q)
         if 1 <= q < 4:
             return q + 1
@@ -36,11 +30,11 @@ class HandleDate(object):
         else:
             raise ValueError('Unknown Quarter with str format : %s ' % str(q))
 
-    def GetCurrentQuarter(self, date=datetime.datetime.now()):
+    def Get_CurrentQuarter(self, date=datetime.datetime.now()):
 
-        return self.getBetweenQuarter(datetime.datetime.strftime(date, "%Y-%m-%d"))[0]
+        return self.Get_BetweenQuarter(datetime.datetime.strftime(date, "%Y-%m-%d"))[0]
 
-    def GetLastMonthinQuarter(self, q):
+    def Get_LastMonthinQuarter(self, q):
         if isinstance(q, str):
             return str(int(q) * 3)
         elif isinstance(q, (int, float)):
@@ -49,7 +43,7 @@ class HandleDate(object):
             raise ValueError(
                 'Unknown Quarter with Wrong format: {},Type: {} '.format(q, type(q)))
 
-    def getBetweenQuarter(self, begin_date):
+    def Get_BetweenQuarter(self, begin_date):
         quarter_list = []
         month_list = self.getBetweenMonth(begin_date)
         for value in month_list:
@@ -75,16 +69,70 @@ class HandleDate(object):
         while begin_date <= end_date:
             date_str = begin_date.strftime("%Y-%m")
             date_list.append(date_str)
-            begin_date = self.add_months(begin_date, 1)
+            begin_date = self.Add_months(begin_date, 1)
         return date_list
 
-    def add_months(self, datetimes, month):
+    def Add_months(self, datetimes, month):
         # auto calculate add month of datetime
         # z + pd.tseries.offsets.DateOffset(months=4, days=5)
 
         return datetimes + pd.tseries.offsets.DateOffset(months=month)
 
-    def Month2StrMonthWithAutoFillup(self, Month, count=0):
+class HandleDate(object):
+    """
+    This class provides the date relative the api service and handle some date
+    format.
+    """
+
+    def __init__(self):
+        pass
+
+    def Handle_Str2DateTime(self, strs, method='datatime', fmt='%Y-%m-%d', **kwargs):
+        """
+        pandas.to_datetime(arg, errors='raise', dayfirst=False, yearfirst=False, utc=None, box=True, format=None, exact=True, unit=None, infer_datetime_format=False, origin='unix', cache=False)
+        :param str:
+        :param method:  datatime or pandas
+        :param fmt: format of datetime
+        :return: datetime format item
+        """
+        if method == 'datetime':
+            import datetime
+            return datetime.datetime.strptime(strs, fmt)
+
+        elif method == 'pandas':
+            import pandas as pd
+
+            return pd.to_datetime(strs, format=fmt)
+        else:
+            print('Unknown method, will use the datetime method')
+            import datetime
+            return datetime.datetime.strptime(strs, fmt)
+
+    def Handle_Str2DateTime_datetimeversion(self, strs, fmt='%Y-%m-%d'):
+        import datetime
+        return datetime.datetime.strptime(strs, fmt)
+
+    def Handle_Str2TimeStamp(self, strs, fmt='%Y-%m-%d'):
+        import datetime,time
+        a = datetime.datetime.strptime(strs, fmt).timetuple()
+        return int(time.mktime(a))
+
+    def Handle_Datetime2Str(self, datetimes, fmt='%Y-%m-%d %H:%M:%S'):
+        return datetimes.strftime(fmt)
+
+    def Handle_Datetime2TimeStamp(self, datetimes):
+
+        return int(time.mktime(datetimes.timetuple()))
+
+    def Handle_Timestamp2Str(self, timestamp, fmt='%Y-%m-%d %H:%M:%S'):
+        import datetime
+        return datetime.datetime.fromtimestamp(timestamp).strftime(fmt)
+
+    def Handle_Timestamp2Datetime(self, timestamp):
+        import datetime
+        return datetime.datetime.fromtimestamp(timestamp)
+
+    def Hanlde_Month2StrMonthWithAutoFillup(self, Month, count=0):
         if isinstance(Month, str):
             if len(Month) == 2:
                 return Month
@@ -95,7 +143,7 @@ class HandleDate(object):
         elif isinstance(Month, (int, float)):
             if count <= 5:
                 count += 1
-                return self.Month2StrMonthWithAutoFillup(str(int(Month)), count)
+                return self.Hanlde_Month2StrMonthWithAutoFillup(str(int(Month)), count)
             else:
                 raise ValueError(
                     'Unknown Situation with Int/float type Month: {}'.format(Month))
@@ -105,5 +153,10 @@ class HandleDate(object):
             raise ValueError('Unknown Month format: %s' % str(Month))
 
 
+class WheelAboutDate(HandleDate, GetDate):
+    pass
+
 if __name__ == '__main__':
+    WAD = WheelAboutDate()
+    print(dir(WAD))
     pass

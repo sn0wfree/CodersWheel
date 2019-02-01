644a1bb93d20b81d2529138834971fc230c24058
sn0wfree <snowfreedom0815@gmail.com>
2019-02-01 16:19:21 +0800

    updated 2019-02-01

---
+++ Tools/Needleman_Wunsch.py
@@ -0,0 +1,189 @@
+# coding=utf8
+try:
+    from inspect import signature
+except ImportError as e:
+    from funcsigs import signature
+import pandas as pd
+from functools import wraps
+
+
+def typeassert(*ty_args, **ty_kwargs):
+    """
+    类型检查(装饰器版本)
+    :param ty_args:
+    :param ty_kwargs:
+    :return:
+    """
+
+    def decorate(func):
+        # If in optimized mode, disable type checking
+        if not __debug__:
+            return func
+
+        # Map function argument names to supplied types
+        sig = signature(func)
+        bound_types = sig.bind_partial(*ty_args, **ty_kwargs).arguments
+
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            bound_values = sig.bind(*args, **kwargs)
+            # Enforce type assertions across supplied arguments
+            for name, value in bound_values.arguments.items():
+                if name in bound_types:
+                    if not isinstance(value, bound_types[name]):
+                        raise TypeError(
+                            'Argument {} must be {}'.format(name, bound_types[name])
+                        )
+            return func(*args, **kwargs)
+
+        return wrapper
+
+    return decorate
+
+
+def Needleman_Wunsch(l1, l2, empty_slot='-'):
+    if not l1:
+        return 'l1 is empty list'
+    elif not l2:
+        return 'l2 is empty list'
+    else:
+        pass
+
+    m = len(l1)
+    n = len(l2)
+    # 初始化
+    lcs = [[i * (-2)] for i in range(0, m + 1)]
+    lcs[0] = [j * (-2) for j in range(0, n + 1)]
+    #
+    for i in range(m):
+        for j in range(n):
+            lcs[i + 1].append(
+                max(lcs[i][j] + (1 if l1[i] == l1[j] else -1), lcs[i][j + 1] - 2, lcs[i + 1][j] - 2, ))
+
+    i = m - 1
+    j = n - 1
+    common_l1 = []
+    common_l2 = []
+    common_l1.insert(0, l1[i])
+    common_l2.insert(0, l2[j])
+
+    while 1:
+        if i == 0 and j == 0:
+            break
+        if l1[i] == l2[j]:
+            if lcs[i - 1][j - 1] + 1 > lcs[i - 1][j] - 2 and lcs[i - 1][j - 1] + 1 > lcs[i][j - 1] - 2:
+                i = i - 1
+                j = j - 1
+                common_l1.insert(0, l1[i])
+                common_l2.insert(0, l2[j])
+                print(1)
+            else:
+                if lcs[i][j + 1] > lcs[i + 1][j]:
+                    i = i - 1
+                    common_l1.insert(0, l1[i])
+                    common_l2.insert(0, empty_slot)
+                    # common_l1 = u"%s%s" % (l1[i], common_l1)
+                    # common_l2 = u"%s%s" % ('-', common_l2)
+
+                else:
+                    j = j - 1
+                    common_l1.insert(0, empty_slot)
+                    common_l2.insert(0, l2[j])
+                    # common_l1 = u"%s%s" % ('-', common_l1)
+                    # common_l2 = u"%s%s" % (l2[j], common_l2)
+
+        else:
+            if lcs[i - 1][j - 1] + 1 > lcs[i - 1][j] - 2 and lcs[i - 1][j - 1] + 1 > lcs[i][j - 1] - 2:
+                i = i - 1
+                j = j - 1
+                common_l1.insert(0, l1[i])
+                common_l2.insert(0, l2[j])
+                # common_l1 = u"%s%s" % (l1[i], common_l1)
+                # common_l2 = u"%s%s" % (l2[j], common_l2)
+
+            else:
+                if lcs[i][j + 1] > lcs[i + 1][j]:
+                    i = i - 1
+                    common_l1.insert(0, l1[i])
+                    common_l2.insert(0, empty_slot)
+                    # common_l1 = u"%s%s" % (l1[i], common_l1)
+                    # common_l2 = u"%s%s" % ('-', common_l2)
+
+                else:
+                    j = j - 1
+                    common_l1.insert(0, empty_slot)
+                    common_l2.insert(0, l2[j])
+                    # common_l1 = u"%s%s" % ('-', common_l1)
+                    # common_l2 = u"%s%s" % (l2[j], common_l2)
+    return common_l1, common_l2
+
+
+def Needleman_Wunsch_others(str1, str2):
+    # 字符串为空则返回
+    if str1 == '' or str2 == '':
+        return ''
+    # 字符串长度
+    m = len(str1)
+    n = len(str2)
+    # 初始化
+    lcs = [[i * (-2)] for i in range(0, m + 1)]
+    lcs[0] = [j * (-2) for j in range(0, n + 1)]
+    #
+    for i in range(m):
+        for j in range(n):
+            lcs[i + 1].append(
+                max(lcs[i][j] + (1 if str1[i] == str2[j] else -1), lcs[i][j + 1] - 2, lcs[i + 1][j] - 2, )
+            )
+    # for i in range(m+1):
+    # print lcs[i]
+    i = m - 1
+    j = n - 1
+    common_substr1 = u''
+    common_substr2 = u''
+    common_substr1 = u"%s%s" % (str1[i], common_substr1)
+    common_substr2 = u"%s%s" % (str2[j], common_substr2)
+    # 回溯
+    while True:
+        if i == 0 and j == 0:
+            break
+        if str1[i] == str2[j]:
+            if lcs[i - 1][j - 1] + 1 > lcs[i - 1][j] - 2 and lcs[i - 1][j - 1] + 1 > lcs[i][j - 1] - 2:
+                i = i - 1
+                j = j - 1
+                common_substr1 = u"%s%s" % (str1[i], common_substr1)
+                common_substr2 = u"%s%s" % (str2[j], common_substr2)
+                print(1)
+            else:
+                if lcs[i][j + 1] > lcs[i + 1][j]:
+                    i = i - 1
+                    common_substr1 = u"%s%s" % (str1[i], common_substr1)
+                    common_substr2 = u"%s%s" % ('-', common_substr2)
+
+                else:
+                    j = j - 1
+                    common_substr1 = u"%s%s" % ('-', common_substr1)
+                    common_substr2 = u"%s%s" % (str2[j], common_substr2)
+
+        else:
+            if lcs[i - 1][j - 1] + 1 > lcs[i - 1][j] - 2 and lcs[i - 1][j - 1] + 1 > lcs[i][j - 1] - 2:
+                i = i - 1
+                j = j - 1
+                common_substr1 = u"%s%s" % (str1[i], common_substr1)
+                common_substr2 = u"%s%s" % (str2[j], common_substr2)
+
+            else:
+                if lcs[i][j + 1] > lcs[i + 1][j]:
+                    i = i - 1
+                    common_substr1 = u"%s%s" % (str1[i], common_substr1)
+                    common_substr2 = u"%s%s" % ('-', common_substr2)
+
+                else:
+                    j = j - 1
+                    common_substr1 = u"%s%s" % ('-', common_substr1)
+                    common_substr2 = u"%s%s" % (str2[j], common_substr2)
+    return common_substr1, common_substr2
+
+
+if __name__ == '__main__':
+    print(Needleman_Wunsch("AGCACACA", "ACACTA"))
+    pass
---
+++ Tools/SeriesComparing.py
@@ -0,0 +1,154 @@
+# coding=utf8
+import copy
+import pandas as pd
+from HSSearchTool_v002 import SearchTools4HSNewMacroDatabase
+from Needleman_Wunsch import Needleman_Wunsch
+from functools import wraps
+
+import inspect
+try:
+    from inspect import signature
+except ImportError as e:
+    from funcsigs import signature
+
+
+def typeassert(*ty_args, **ty_kwargs):
+    """
+    类型检查(装饰器版本)
+    :param ty_args:
+    :param ty_kwargs:
+    :return:
+    """
+
+    def decorate(func):
+        # If in optimized mode, disable type checking
+        if not __debug__:
+            return func
+
+        # Map function argument names to supplied types
+        sig = signature(func)
+        bound_types = sig.bind_partial(*ty_args, **ty_kwargs).arguments
+
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            bound_values = sig.bind(*args, **kwargs)
+            # Enforce type assertions across supplied arguments
+            for name, value in bound_values.arguments.items():
+                if name in bound_types:
+                    if not isinstance(value, bound_types[name]):
+                        raise TypeError(
+                            'Argument {} must be {}'.format(name, bound_types[name])
+                        )
+            return func(*args, **kwargs)
+
+        return wrapper
+
+    return decorate
+
+
+class DataPoint(object):
+    def __init__(self, label, value):
+        self.__label_info = label
+        self.__value_info = value
+
+    @property
+    def label(self):
+        return self.__label_info
+
+    @property
+    def value(self):
+        return self.__value_info
+
+
+class DataSett(object):
+
+    @classmethod
+    @typeassert(object, series=pd.Series)
+    def _transform(cls, series):
+        index_list = series.index
+        value_list = series.values
+        return list(cls._transform_with_label(index_list, value_list))
+
+    @staticmethod
+    def _transform_with_label(index_list, value_list):
+        for items in zip(index_list, value_list):
+            yield DataPoint(*items)
+
+    @staticmethod
+    @typeassert(DataSettlist=list, label_name=str, value_name=str)
+    def _parse_datapoint(DataSettlist, label_name=None, value_name=None):
+        label_name = 'label' if label_name is None else label_name
+        value_name = 'value' if value_name is None else value_name
+
+        info = ((item.label, item.value) for item in DataSettlist)
+        return pd.DataFrame(info, columns=[label_name, value_name])
+
+    @classmethod
+    @typeassert(object, pd.Series, label_name=str, value_name=str)
+    def _parse(cls, series, label_name=None, value_name=None):
+        return cls._transform(series)
+
+
+class CheckSeries(object):
+
+    def __init__(self, test_series, **src_series):
+        self.test_series = test_series
+        self.src_series = src_series
+
+        self.length = len(test_series)
+        self.length_src = [len(sr) for sr in self.src_series]
+        pass
+
+    @staticmethod
+    @typeassert(test_series=pd.Series, test_name=str, compare_series=pd.Series, compare_name=str, empty_slot=DataPoint)
+    def _do_NM(test_series, test_name, compare_series, compare_name, empty_slot=None):
+        empty_slot = DataPoint('inserted', '-') if empty_slot is None else empty_slot
+        dp_test_series = DataSett._parse(test_series)
+        dp_compare_series = DataSett._parse(compare_series)
+        NW_test, NM_compared = Needleman_Wunsch(dp_test_series, dp_compare_series, empty_slot=empty_slot)
+        NW_test_df = DataSett._parse_datapoint(NW_test, value_name=test_name,label_name=test_name+'label')
+        NM_compared_df = DataSett._parse_datapoint(NM_compared, value_name=compare_name,label_name=compare_name+'label')
+
+        wd = pd.concat([NW_test_df, NM_compared_df], axis=1)
+        wd['cmp'] = (wd[wd.columns[1]] != wd[wd.columns[3]]) * 1
+
+        return wd
+
+    @staticmethod
+    def _parse_numeric_compare_error(numeric_compare):
+        print(numeric_compare)
+
+    @staticmethod
+    @typeassert(test_series=pd.Series, compare_series=pd.Series)
+    def _get_coverage(test_series, compare_series):
+        lentest_series = len(test_series)
+        lensrc_series = len(compare_series)
+        if lentest_series == lensrc_series and not test_series.empty and not compare_series.empty:
+            numeric_compare = (test_series == test_series) * 1
+            boolsum = numeric_compare.sum()
+            coverage = boolsum * 1.0 / lentest_series
+            return coverage, boolsum, numeric_compare
+        else:
+            if test_series.empty and compare_series.empty:
+                raise ValueError('test series and compare series both are empty!')
+            elif test_series.empty:
+                raise ValueError('test series is empty!')
+            elif compare_series.empty:
+                raise ValueError('compare series is empty!')
+            elif lentest_series > lensrc_series:
+                raise ValueError('test series owns more rows than compare series!')
+            elif lentest_series < lensrc_series:
+                raise ValueError('compare series owns more rows than test series!')
+            else:
+                raise Exception('Unknown　Error!')
+
+
+if __name__ == '__main__':
+    hs = SearchTools4HSNewMacroDatabase('hs')
+    testdata = hs.sql2data('select ID,ContractInnerCode from Fut_TradingQuote limit 100')
+    copied_testdata = copy.deepcopy(testdata)
+    CheckSeries._get_coverage(testdata.ID, copied_testdata.ID)
+    wd = CheckSeries._do_NM(testdata.ID, 'testid', copied_testdata[:97].ID, 'comparedid')
+
+    # print(Needleman_Wunsch("AGCACACA", "ACACTA"))
+    pass

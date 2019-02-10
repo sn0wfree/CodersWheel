75ab6eece593c3774597f06a3d94c0341ece35ab
sn0wfree <snowfreedom0815@gmail.com>
2019-02-11 01:30:44 +0800

    add async version of mysql conenctor
    add async version of mysql conenctor

--- ConnectorWheel/db_core/asnyc_mysql_connector.py
+++ ConnectorWheel/db_core/asnyc_mysql_connector.py
@@ -1,4 +1,125 @@
 # coding=utf8
 import asyncio
-import aiomysql
-from aiomysql import create_pool
+import aiomysql, warnings
+import time, random, gevent
+from functools import reduce
+import pandas as pd
+
+
+# @asyncio.coroutine
+# def test_example(loop):
+#     conn = yield from aiomysql.connect(host='112.74.189.154', port=3306,
+#                                        user='linlu', password='Imsn0wfree', db='mysql',
+#                                        loop=loop)
+#
+#     cur = yield from conn.cursor()
+#     yield from cur.execute("SELECT Host,User FROM user")
+#     print(cur.description)
+#     (r,) = yield from cur.fetchall()
+#     print(r)
+#     yield from cur.close()
+#     conn.close()
+#
+# loop = asyncio.get_event_loop()
+# loop.run_until_complete(test_example(loop))
+
+
+class AsyncMysql(object):
+    @staticmethod
+    def chunks(l, chunksize, raise_error=True, critical_level=500):
+        """Yield successive n-sized chunks from l."""
+        # print(len(l)/ chunksize)
+        if raise_error and len(l) / chunksize >= critical_level:
+            raise ValueError('chunksize is too small or taskspool is too bigger!')
+        elif raise_error is False and len(l) / chunksize >= critical_level:
+            import math
+            chunksize = math.ceil(len(l) / critical_level)
+        else:
+            pass
+
+        for i in range(0, len(l), chunksize):
+            yield l[i:i + chunksize]
+
+    @staticmethod
+    async def async_for(iterablelist):
+        for sql in iterablelist:
+            await asyncio.sleep(1)
+            yield sql
+
+    @staticmethod
+    async def test_cursor_execute_func(pool, sql):
+        async with pool.acquire() as conn:
+            async with conn.cursor() as cur:
+                # async for sql_result in cur_execute(cur, sql_set):
+                #     holder.append(sql_result)
+
+                await cur.execute(sql)
+                sql_result = await cur.fetchall()
+                col = list(map(lambda x: x[0], cur.description))
+                print(time.time(), sql_result)
+                return sql, col, sql_result
+
+    @classmethod
+    async def cur_execute(cls, loop, mysqlpara, sqlset):
+        pool = await aiomysql.create_pool(**mysqlpara, loop=loop)
+        holder = [await cls.test_cursor_execute_func(pool, sql) for sql in sqlset]
+        # holder = [await  test_example(pool, sql) async for sql in async_for(sqlset)]
+
+        pool.close()
+        await pool.wait_closed()
+        return holder
+
+    @staticmethod
+    def parse_result(res, to_df=False, merge=False):
+        from itertools import chain
+        res = chain(*list(map(lambda x: x._result, res[0])))
+        if to_df:
+            res = ((sql, col, pd.DataFrame(list(data), columns=col)) for sql, col, data in res)
+            if merge:
+                return pd.concat(map(lambda x: x[-1], res))
+            else:
+                return res
+        else:
+            if merge:
+                warnings.warn('to_df is False, switch merge to False forced!')
+            return list(res)
+
+    # assert r == 42
+    @classmethod
+    def main_query(cls, mysqlpara, sqlset, chunksize=100, to_df=False, merge=False):
+        """
+        the main function of query executing function with async coroutine version
+        :param mysqlpara:  mysql parameter
+        :param sqlset:  a set or a list of sql
+        :param chunksize:  chunk size of group
+        :param to_df:  whether to dataframe
+        :param merge:  whether merge
+        :return: a tuple of data
+        """
+        loop = asyncio.get_event_loop()
+        # [cur_execute(loop, mysqlpara,sql)  for sql in chunks(sqlset,5) ]
+
+        # s = loop.run_until_complete(asyncio.wait([cur_execute(loop, mysqlpara, sqlset),
+        #                                           cur_execute(loop, mysqlpara, sqlset10),
+        #                                           cur_execute(loop, mysqlpara, sqlset20)]))
+
+        s = loop.run_until_complete(
+            asyncio.wait([cls.cur_execute(loop, mysqlpara, sql) for sql in cls.chunks(sqlset, chunksize)]))
+
+        result = cls.parse_result(s, to_df=to_df, merge=merge)
+        loop.close()
+        return result
+
+
+if __name__ == '__main__':
+    mysqlpara = dict(host='112.74.189.154',
+                     port=3306,
+                     user='linlu',
+                     password='Imsn0wfree',
+                     db='mysql')
+
+    sqlset = ["SELECT {} as col, 1 as test;".format(i) for i in range(100)]
+
+    result = AsyncMysql.main_query(mysqlpara, sqlset, chunksize=10, to_df=False, merge=False)
+
+    print(1)
---
+++ OtherToolWheel/coroutine_prev_active_decor.py
@@ -0,0 +1,33 @@
+# coding=utf8
+""" This pytho file is the prev active coroutine  for all kind of coroutine
+"""
+from functools import wraps
+
+
+def prev_active_coroutine(func):
+    """
+    预激协程的装饰器
+    """
+    @wraps(func)
+    def primer(*args, **kwargs):
+        gen = func(*args, **kwargs)
+        next(gen)
+        return gen
+    return primer
+
+
+if __name__ == '__main__':
+
+    @prev_active_coroutine
+    def averager2():
+        """calc moving average
+        """
+        total = 0.0
+        count = 0
+        average = None
+        while True:
+            term = yield average
+            total += term
+            count += 1
+            average = total / count
+    pass

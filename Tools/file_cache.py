bcd481134c1374bab5798cadefd5db2f424d6c2c
sn0wfree <snowfreedom0815@gmail.com>
2019-12-22 12:33:34 +0800

    alter structure and debug

--- Tools/file_cache.py
+++ Tools/file_cache.py
@@ -1,48 +1,58 @@
-#coding=utf-8
+# coding=utf-8
 import hashlib
 import os
-import pandas as pd
-import pcikle
-from collection import OrderedDict
+import datetime
+import pickle
+from collections import OrderedDict
+from functools import wraps
 
-def get_cache_path():
-	__cache__path = '/tmp/{}/'.format(pd.datetime.now().strftime(format="%Y-%m-%d"))
-	if os.path.exist(__cache__path):
-		pass
-	else:
-		os.mkdir(__cache__path)
-	return __cache__path
-	
-	
-def _cache(func,arg,kwargs,time_format_dimension='%Y-%m-%d'):
-	kwargs = OrderedDict(sorted(kwargs.items(),key=lambda t :t[0]))
-	key = pickle.dumps([func.__code__._co_name,arg,kwargs])
-	name = func.__code__._co_name+"_" + hashlib.sha1(key).hexdigest()+"_{}".format(pd.datetime.now().strftime(time_format_dimension))
-	file_path = get_cache_path()
-	if os.path.exist(file_path+name):
-		with open(file_path+name,'rb') as f:
-			res = pickle.load(f)
-	else:
-		res = func(*arg,**kwargs)
-		with open(file_path+name,'wb') as f:
-			pickle.sump(res,f)
-	return res
-		
-		
+
+def get_cache_path(fmt="%Y-%m-%d"):
+    __cache__path = '/tmp/{}/'.format(datetime.datetime.now().strftime(format=fmt))
+    if not os.path.exists(__cache__path):
+        os.mkdir(__cache__path)
+    return __cache__path
+
+
+def prepare_args(func, arg, kwargs, time_format_dimension='%Y-%m-%d'):
+    kwargs = OrderedDict(sorted(kwargs.items(), key=lambda t: t[0]))  # sort kwargs to fix hashcode if sample input
+    func_name = func.__code__.co_name
+    key = pickle.dumps([func_name, arg, kwargs])  # get the unique key for the same input
+    name = func_name + "_" + hashlib.sha1(key).hexdigest() + "_{}".format(
+        datetime.datetime.now().strftime(time_format_dimension))  # create cache file name
+    file_path = get_cache_path()
+    return file_path, name
+
+
+def _cache(func, arg, kwargs, time_format_dimension='%Y-%m-%d'):
+    file_path, name = prepare_args(func, arg, kwargs, time_format_dimension=time_format_dimension)
+    if os.path.exists(file_path + name):
+        with open(file_path + name, 'rb') as f:
+            res = pickle.load(f)
+    else:
+        res = func(*arg, **kwargs)
+        with open(file_path + name, 'wb') as f:
+            pickle.dump(res, f)
+    return res
 
 
 def file_cache(deco_arg_dict):
-	if callable(deco_arg_dict):
-		@wraps(deco_arg_dict)
-		def wrapped(*args,**kwargs):
-			return _cache(deco_arg_dict,args,kwargs)
-		return wrapped
-	else:
-		def _deco(func):
-			@wraps(func)
-			def __deco(*args,**kwargs):
-				return _cache(func,args,kwargs)
-			return __deco
-		return _deco
-if __name__=='__main__':
-	pass
+    if callable(deco_arg_dict):
+        @wraps(deco_arg_dict)
+        def wrapped(*args, **kwargs):
+            return _cache(deco_arg_dict, args, kwargs)
+
+        return wrapped
+    else:
+        def _deco(func):
+            @wraps(func)
+            def __deco(*args, **kwargs):
+                return _cache(func, args, kwargs)
+
+            return __deco
+
+        return _deco
+
+
+if __name__ == '__main__':
+    pass
